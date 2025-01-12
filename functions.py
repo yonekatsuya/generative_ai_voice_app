4c3eb9f4e9604a7a68bb8456d755f231ab1b0b17
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2025-01-13 00:18:04 +0900

    fix

--- functions.py
+++ functions.py
@@ -25,7 +25,7 @@ def record_audio(fs=48000, dir="audio/input", silence_threshold=2.5, min_duratio
     progress_bar = st.progress(progress_num)
 
     print("現在使用中のデバイス一覧")
-    print(sd.query_devices())
+    # print(sd.query_devices())
 
     with sd.InputStream(samplerate=fs, channels=2) as stream:
         while True:
